0a829da8f6380a5f5c7af7a2039d69a87a37becd
Ahmmad Ismail <ahmmad.ismail@yandex.com>
2024-05-24 22:53:12 +0600

    Adornment and Title

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const mathjaxScript = fs.readFileSync('mathjax_script.html', 'utf8');
 // Example usage
 const parser = new MarkdownParser('rules.yaml');
 const markdownText = `
-# Heading level 1
-## Heading level 2
-### Heading level 3
-#### Heading level 4
-##### Heading level 5
-###### Heading level 6
+= Heading level 1
+== Heading level 2
+=== Heading level 3
+==== Heading level 4
+===== Heading level 5
+====== Heading level 6
 
 .Feeding the Werewolves
 [NOTE]
